49f50b7e1570faa7d9dc4fc8ad8c4ad5064453c7
Patrick Connolly <patrick@myplanetdigital.com>
2013-08-28 18:21:26 -0400

    Adds domain-related api calls.

--- lib/acapi/client/domains.rb
+++ lib/acapi/client/domains.rb
@@ -2,9 +2,28 @@ module AcquiaCloudApi
   class Client
     module Domains
 
-      def create_domain(domain, env)
+      def domains(env)
+        get "sites/#{@site_name}/envs/#{env}/domains"
+      end
+      alias :list_domains :domains
+
+      def domain(domain, env)
+        get "sites/#{@site_name}/envs/#{env}/domains/#{domain}"
+      end
+
+      def add_domain(domain, env)
         post "/sites/#{@site_name}/envs/#{env}/domains/#{domain}"
       end
+
+      def remove_domain(domain, env)
+        delete "sites/#{@site_name}/envs/#{env}/domains/#{domain}"
+      end
+
+      def purge_varnish_cache(domain, env)
+        delete "sites/#{@site_name}/envs/#{env}/domains/#{domain}/cache"
+      end
+      alias :purge_varnish :purge_varnish_cache
+      alias :purge_cache   :purge_varnish_cache
 
     end
   end
