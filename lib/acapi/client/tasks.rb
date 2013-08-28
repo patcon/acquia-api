1635e747c4436ada5987411c1c77697216481cde
Patrick Connolly <patrick@myplanetdigital.com>
2013-08-28 17:59:35 -0400

    Fixes task-related api calls.

--- lib/acapi/client/tasks.rb
+++ lib/acapi/client/tasks.rb
@@ -7,15 +7,20 @@ module AcquiaCloudApi
       end
       alias :list_tasks :tasks
 
+      def task(task_id)
+        get "sites/#{@site_name}/tasks/#{task_id}"
+      end
+
       def task_status(task_id)
-        res = get "tasks/#{task_id}"
+        res = task(task_id)
 
         res['state']
       end
 
       def task_complete?(task_id)
-        task_status == "done"
+        task_status(task_id) == "done"
       end
+      alias :task_done? :task_complete?
 
       def poll_task(task_id)
         time = 0
